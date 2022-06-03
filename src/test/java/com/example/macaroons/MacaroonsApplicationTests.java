0365bd804a8107c28415f1cf85a9c455b2b828b2
aminatd <94138060+aminatd@users.noreply.github.com>
2022-06-03 04:35:31 -0600

    Initial commit

---
+++ src/test/java/com/example/macaroons/MacaroonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.macaroons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacaroonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

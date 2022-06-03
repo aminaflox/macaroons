9ce76ccc4bfe05b185a42769a59dae9422ddedcc
aminaflox <98947733+aminaflox@users.noreply.github.com>
2022-06-03 05:02:12 -0600

    Update testController.java

--- src/main/java/com/example/macaroons/testController.java
+++ src/main/java/com/example/macaroons/testController.java
@@ -1,2 +1,16 @@
 package com.example.macaroons;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class testController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MacaroonsApplication.class, args);
+    }
+    @GetMapping(value = "/")
+    public String aName(){
+        return "Login.html";
+    }
+}
